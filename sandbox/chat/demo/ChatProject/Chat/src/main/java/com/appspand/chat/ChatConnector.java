84babbc40d73fb5d5df4695727eb4a578a8d6237
Jaepil Jeong <jaepil@kaist.ac.kr>
2013-07-15 11:47:08 +0900

    디렉토리 및 파일 권한 변경

---
+++ sandbox/chat/demo/ChatProject/Chat/src/main/java/com/appspand/chat/ChatConnector.java
@@ -0,0 +1,7 @@
+package com.appspand.chat;
+
+/**
+ * Created by jaepil on 7/13/13.
+ */
+public class ChatConnector {
+}
---
+++ sandbox/chat/demo/ChatProject/Chat/src/main/java/com/appspand/chat/com/appspand/chat/connector/WebSocketClient.java
@@ -0,0 +1,7 @@
+package com.appspand.chat.com.appspand.chat.connector;
+
+/**
+ * Created by jaepil on 7/13/13.
+ */
+public class WebSocketClient {
+}
