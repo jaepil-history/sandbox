4532383278419a4c72469200aa2007bc5136894a
Jaepil Jeong <jaepil@kaist.ac.kr>
2013-09-29 18:28:15 +0900

    copyright 수정

---
+++ appengine/appspand/appengine/idgen/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2013 Appspand, Inc.
+
+from connector import get_next_id
+from connector import get_next_ids
+
+
+__all__ = ["get_next_id", "get_next_ids"]
