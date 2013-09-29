70b316e505ca23baf21770e554d6fc443bcf6df3
Jaepil Jeong <jaepil@kaist.ac.kr>
2013-09-29 18:28:33 +0900

    Revert "copyright 수정"
    This reverts commit 4532383278419a4c72469200aa2007bc5136894a.

--- appengine/appspand/appengine/idgen/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright (c) 2013 Appspand, Inc.
-
-from connector import get_next_id
-from connector import get_next_ids
-
-
-__all__ = ["get_next_id", "get_next_ids"]
--- appengine/appspand/appengine/probe/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright (c) 2013 Appspand, Inc.
